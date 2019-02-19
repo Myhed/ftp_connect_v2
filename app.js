836419028677d650f4e8091f01c5ed122d0541d4
Myhed <capuchondu92@hotmail.fr>
2019-02-19 18:34:26 +0100

    refactor code

--- app.js
+++ app.js
@@ -1,50 +1,76 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const PromiseFtp = require('promise-ftp');
-
-const app = express()
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
+const express = require("express");
+const bodyParser = require("body-parser");
+const PromiseFtp = require("promise-ftp");
+const morgan = require("morgan");
+const app = express();
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 const ftp = new PromiseFtp();
-async function ftpConnect(options={}){
-    return await ftp.connect(options)
-}
 
-app.use('/',async (req,res,next) => {
-    if(ftp.getConnectionStatus() !== "connected"){
-        try{
-            connect = await ftpConnect(req.body)
-        }catch(err){
-            next(err)
-        }
-        res.send("connected")
-    }
-    next()
-})
+app.use( async (req,res,next) => {
+	// Website you wish to allow to connect
+	res.setHeader("Access-Control-Allow-Origin", "*");
+    
+	// Request methods you wish to allow
+	res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+    
+	// Request headers you wish to allow
+	res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type");
+    
+	// Set to true if you need the website to include cookies in the requests sent
+	// to the API (e.g. in case you use sessions)
+	res.setHeader("Access-Control-Allow-Credentials", true);
+	next();
+});
 
-app.use(['/',/[/]+[:\w\W]*/],async (req,res,next) => {
-    try{
-        getDirectory = await ftp.list(req.url);
-        req.directory = getDirectory
-    }catch(err){
-        next(err)
-    }
-    next()
-})
+app.use("/",async (req,res,next) => {
+	let error = {};
+	const status = ftp.getConnectionStatus();
+	if(status != "connected" && (!req.body.host || !req.body.user || !req.body.password)){
+		error.status = 400;
+		error.message = "Bad Request";
+		next(error);
+	}else if(status != "connected" && req.method !== "POST"){
+		error.status = 404;
+		error.message = "Not Found";
+		next(error);
+	}else{
+		if(ftp.getConnectionStatus() !== "connected"){
+			try{
+				await ftp.connect(req.body);
+			}catch(err){
+				error = err;
+			}
+		}
+	}
+	res.status = "connected";
+	next();
+});
 
-app.get('/', async (req,res,next) => {
-    res.send(JSON.stringify(req.directory))
-})
+app.use(["/",/[/]+[:\w\W]*/],async (req,res,next) => {
+	try{
+		const getDirectory = await ftp.list(req.url);
+		req.directory = getDirectory;
+	}catch(err){
+		next(err);
+	}
+	next();
+});
 
-app.get(/[/]+[:\w\W]*/,(req,res,next) => {
-    res.send(`${JSON.stringify(req.directory)} matched`)
-})
+app.get("/", async (req,res) => {
+	res.send(JSON.stringify(req.directory));
+});
 
-app.use('/',(err,req,res,next) => {
-    res.send({message:err.message,status:err.code})
-})
+app.get(/[/]+[:\w\W]*/,(req,res) => {
+	res.send(`${JSON.stringify(req.directory)} matched`);
+});
+
+app.use("/",(err,req,res,next) => {
+	res.send({message:err.message,status:err.code});
+});
 
 
 app.listen(3000, () => {
-    console.log("server is started")
-})
+	console.log("server is started");
+});
